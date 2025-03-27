0819a08420826ecb155a1c5cd2b682c97d28bf8a
Sebastian Lindau-Skands <sebastian.lindauskands@proton.me>
2025-03-27 13:37:30 +0100

    fixed arm64 integration???

--- obj/Debug/net9.0/database.AssemblyInfo.cs
+++ obj/Debug/net9.0/database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f9ac969acb6ae3596198a663c28cba4b61cbc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a23a62bacf713c5008fd6fd945724acda1e45671")]
 [assembly: System.Reflection.AssemblyProductAttribute("database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
